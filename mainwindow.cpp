487f2e6b55d6546823ccd8637374a14ae3162785
yuancong2 <1290667589@qq.com>
2024-10-17 13:32:20 +0800

    part2-数字键输入处理

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,48 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->btnNum0, SIGNAL(clicked()), this, SLOT(btnNumClicked()));
+    connect(ui->btnNum1, SIGNAL(clicked()), this, SLOT(btnNumClicked()));
+    connect(ui->btnNum2, SIGNAL(clicked()), this, SLOT(btnNumClicked()));
+    connect(ui->btnNum3, SIGNAL(clicked()), this, SLOT(btnNumClicked()));
+    connect(ui->btnNum4, SIGNAL(clicked()), this, SLOT(btnNumClicked()));
+    connect(ui->btnNum5, SIGNAL(clicked()), this, SLOT(btnNumClicked()));
+    connect(ui->btnNum6, SIGNAL(clicked()), this, SLOT(btnNumClicked()));
+    connect(ui->btnNum7, SIGNAL(clicked()), this, SLOT(btnNumClicked()));
+    connect(ui->btnNum8, SIGNAL(clicked()), this, SLOT(btnNumClicked()));
+    connect(ui->btnNum9, SIGNAL(clicked()), this, SLOT(btnNumClicked()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::btnNumClicked()
+{
+
+    QString str = ui->display->text();
+    str += qobject_cast<QPushButton *>(sender())->text();
+
+    ui->display->setText(str);
+    // ui->statusbar->showMessage(qobject_cast<QPushButton*>(sender())->text() + "btn clicked");
+
+}
+
+void MainWindow::on_btnPeriod_clicked()
+{
+    QString str = ui->display->text();
+    if (!str.contains("."))
+        str += qobject_cast<QPushButton *>(sender())->text();
+    ui->display->setText(str);
+}
+
+
+void MainWindow::on_btnDel_clicked()
+{
+    QString str = ui->display->text();
+    str = str.left(str.length() - 1);
+    ui->display->setText(str);
+}
+
